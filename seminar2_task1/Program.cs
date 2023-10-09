2759dde6e6ec092a2ceafb346670de178c3002f2
masha <hanchuk91@gmail.com>
2023-10-09 21:35:15 +0300

    Seminar2

---
+++ seminar2_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+Console.WriteLine("Введите треx3начное число: ");
+string numb = Console.ReadLine();
+int number;
+number = int.Parse(numb);
+if(number<<1000 & number>99)
+    {
+        int lastDigit = number%10;
+        Console.WriteLine ("lastDigit="+lastDigit);
+    }
+else
+        Console.WriteLine("LastDigit not three digits");
---
+++ seminar2_task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8 
+//12-> 2 
+//85 -> 8
+int number = new Random().Next(10,100); //Сгенерировали случайное число в range от 10 до 99 и переместили в переменную number
+Console.WriteLine("Number = " + number);
+int firstDigit = number / 10;
+int lastDigit = number % 10;
+if (firstDigit > lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(lastDigit);
+}
